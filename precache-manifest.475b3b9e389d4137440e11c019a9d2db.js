6033c7efb1a9d9c9ab239c0780bbc1c2ecbe1152
Inacio Cerqueira <inaciogcerqueira@gmail.com>
2020-02-13 20:46:43 -0300

    Updates

---
+++ precache-manifest.475b3b9e389d4137440e11c019a9d2db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e0403ca0eb2198c9d35b12dccd8158",
+    "url": "/my-awesome-app/index.html"
+  },
+  {
+    "revision": "f302a3055f0ee824a848",
+    "url": "/my-awesome-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "76bdea3c87ead82a2c79",
+    "url": "/my-awesome-app/static/js/2.c93e7a4b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-awesome-app/static/js/2.c93e7a4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f302a3055f0ee824a848",
+    "url": "/my-awesome-app/static/js/main.75ed22ce.chunk.js"
+  },
+  {
+    "revision": "dcae5c85ffba62029b87",
+    "url": "/my-awesome-app/static/js/runtime-main.5d63b498.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-awesome-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
